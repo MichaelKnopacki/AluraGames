2da16782ab75ab7fbab7539d263ab74b6463c276
Michael Knopacki <michaelknopacki@hotmail.com>
2024-08-10 09:34:21 -0300

    20240810 - Criada as variáveis

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function alterarStatus(id){
+    let game = document.getElementById(`game-${id}`);
+    let imagem = document.querySelector('.dashboard__item__img');
+    let botao = document.querySelector('.dashboard__item__button');
+    let teste = document.querySelector('.dashboard__item__name');
+
+    //Testando ação do botão.
+    alert(teste.textContent);
+}
